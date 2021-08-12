24e5eef9996dd6cca9c2f2525b5ad22a02b2ab51
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-12 09:16:30 +0200

    components form and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ({ titulo }) => {
+  return (
+    <div>
+      <a href="#!">{titulo}</a>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css";
+import React from "react";
+import Header from "../components/Header";
+import Formulario from "../components/Formulario";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header titulo="Weather App" />
+      <Formulario />
+    </div>
+  );
+}
