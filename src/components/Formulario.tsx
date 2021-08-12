ffc3e25bf04b9f19661b1d214cabfd275c13c994
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-12 12:42:28 +0200

    useState

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const { ciudad, pais } = busqueda;
+
+  const handleChange = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div>
         <label htmlFor="ciudad">Ciudad:</label>
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
       </div>
 
       <div>
         <label htmlFor="pais">País:</label>
-        <select name="pais" id="pais">
+        <select
+          name="pais"
+          id="pais"
+          value={pais}
+          onChange={handleChange}
+        >
           <option value="">--Seleccione un país--</option>
           <option value="US">Estados Unidos</option>
           <option value="MX">México</option>
