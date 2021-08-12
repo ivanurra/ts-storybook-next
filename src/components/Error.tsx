62f54f50cca7372526060f8d7cbc13717e27da91
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-12 16:03:05 +0200

    logic done

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = ({ mensaje }) => {
+
+  return (
+    <div>
+      <h3>{mensaje}</h3>
+    </div>
+  );
+};
+
+export default Error;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Formulario from "../components/Formulario";
 import Clima from "../components/Clima";
+import Error from "../components/Error";
 
 export default function Home() {
-
   // STATE
   const [busqueda, guardarBusqueda] = useState({
     ciudad: "",
@@ -16,40 +16,51 @@ export default function Home() {
 
   const [resultado, guardarResultado] = useState({});
 
-  const {ciudad, pais} = busqueda;
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
 
   // API KEY - SIGN UP TO USE THE API: https://openweathermap.org/api
   const NEXT_PUBLIC_SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY;
-  
+
   useEffect(() => {
     const consultarAPI = async () => {
-      
-      if(consultar) {
-
+      if (consultar) {
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${NEXT_PUBLIC_SECRET_KEY}`;
 
         const respuesta = await fetch(url);
-  
+
         const resultado = await respuesta.json();
-  
+
         guardarResultado(resultado);
         guardarConsultar(false);
+
+        if (resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
       }
-    }
+    };
     consultarAPI();
-  },[consultar]);
+  }, [consultar]);
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No se encontró la ciudad. Inténtelo de nuevo." />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
 
   return (
     <div className={styles.container}>
       <Header titulo="Weather App" />
-      <Formulario 
+      <Formulario
         busqueda={busqueda}
         guardarBusqueda={guardarBusqueda}
         guardarConsultar={guardarConsultar}
       />
-      <Clima
-        resultado={resultado}
-      />
+      {componente}
     </div>
   );
 }
